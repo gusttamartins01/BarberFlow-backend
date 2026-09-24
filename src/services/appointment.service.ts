3a3dbe5154c10fddd0c202620ec3ddbe4646a257
gustavo martins <luizgustams@gmail.com>
2026-09-24 18:50:34 -0300

    feat: criar logica da camada de serviços de agendamentos e clientes.

---
+++ src/services/appointment.service.ts
@@ -0,0 +1,66 @@
+import { NotFoundError } from '../errors/index.ts';
+import prisma from '../lib/prisma.ts';
+import type {
+	CreateAppointment,
+	UpdateAppointment
+} from '../schemas/appointment.schema.ts';
+import type { Appointment } from '../types.ts';
+
+function prepareCreateData(data: CreateAppointment) {
+	return {
+		...data,
+		date: new Date(`${data.date}T00:00:00.000Z`)
+	};
+}
+
+function prepareUpdateData(data: UpdateAppointment) {
+	return {
+		...data,
+		...(data.date ? { date: new Date(`${data.date}T00:00:00.000Z`) } : {})
+	};
+}
+
+export async function findAllAppointments(): Promise<Appointment[]> {
+	const appointments = await prisma.appointment.findMany();
+
+	return appointments;
+}
+
+export async function findAppointmentById(id: number): Promise<Appointment> {
+	const appointment = await prisma.appointment.findUnique({
+		where: { id }
+	});
+
+	if (!appointment)
+		throw new NotFoundError(`Agendamento com ID ${id} não encontrado.`);
+
+	return appointment;
+}
+
+export async function insertAppointment(
+	data: CreateAppointment
+): Promise<Appointment> {
+	return await prisma.appointment.create({
+		data: prepareCreateData(data)
+	});
+}
+
+export async function modifyAppointment(
+	id: number,
+	data: UpdateAppointment
+): Promise<Appointment> {
+	await findAppointmentById(id);
+
+	return await prisma.appointment.update({
+		where: { id },
+		data: prepareUpdateData(data)
+	});
+}
+
+export async function removeAppointment(id: number): Promise<void> {
+	await findAppointmentById(id);
+
+	await prisma.appointment.delete({
+		where: { id }
+	});
+}
---
+++ src/services/customer.service.ts
@@ -0,0 +1,50 @@
+import { NotFoundError } from '../errors/index.ts';
+import prisma from '../lib/prisma.ts';
+import type {
+	CreateCustomer,
+	UpdateCustomer
+} from '../schemas/customer.schema.ts';
+import type { Customer } from '../types.ts';
+
+export async function findAllCustomers(): Promise<Customer[]> {
+	const customers = await prisma.customer.findMany();
+
+	return customers;
+}
+
+export async function findCustomerById(id: number): Promise<Customer> {
+	const customer = await prisma.customer.findUnique({
+		where: { id }
+	});
+
+	if (!customer)
+		throw new NotFoundError(`Usuário com ID ${id} não encontrado.`);
+
+	return customer;
+}
+
+export async function insertCustomer(data: CreateCustomer): Promise<Customer> {
+	return await prisma.customer.create({
+		data
+	});
+}
+
+export async function modifycustomer(
+	id: number,
+	data: UpdateCustomer
+): Promise<Customer> {
+	await findCustomerById(id);
+
+	return await prisma.customer.update({
+		where: { id },
+		data
+	});
+}
+
+export async function removeCustomer(id: number): Promise<void> {
+	await findCustomerById(id);
+
+	await prisma.customer.delete({
+		where: { id }
+	});
+}
