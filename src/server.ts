78511e1bfd0c59eda1b1bc975dde8eb805853037
gustavo martins <luizgustams@gmail.com>
2026-09-26 01:38:48 -0300

    refactor: adiconar novas rotas no servidor principal.

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import { pinoHttp } from 'pino-http';
 import logger from './lib/logger.ts';
 import errorHandler from './middlewares/errorhandler.ts';
 import AppointmentRouter from './routes/appointment.route.ts';
+import BarberRouter from './routes/barber.route.ts';
+import BusinessHours from './routes/businessHours.route.ts';
+import ComboRouter from './routes/combo.route.ts';
 import CustomerRouter from './routes/customer.route.ts';
+import ServiceRouter from './routes/service.route.ts';
 
 const app = express();
 
@@ -11,8 +15,12 @@ app.use(pinoHttp({ logger }));
 
 app.use(express.json());
 
-app.use('/customers', CustomerRouter);
 app.use('/appointments', AppointmentRouter);
+app.use('/barbers', BarberRouter);
+app.use('/business-hours', BusinessHours);
+app.use('/combos', ComboRouter);
+app.use('/customers', CustomerRouter);
+app.use('/services', ServiceRouter);
 
 app.use((_request, response) => {
 	response.status(404).json({
@@ -25,5 +33,5 @@ app.use(errorHandler);
 const PORT = Number(process.env.PORT);
 
 app.listen(PORT, () => {
-	console.log(`Servidor rodando na porta: http://localhost:${PORT}`);
+	logger.info(`Servidor rodando na porta: http://localhost${PORT}`);
 });
